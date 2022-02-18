d8956e468c5b84a74fc0ca55a9fd97002bd50bec
dwiky maulana <adwikymln@gmail.com>
2022-02-18 21:47:47 +0700

    add swift file challenge

---
+++ Contents.swift
@@ -0,0 +1,44 @@
+import UIKit
+
+var greeting = "Hello, playground"
+/* Untuk meningkatkan skill teman-teman dalam mempelajari
+ pengembangan aplikasi iOS, kita perlu untuk mempraktekkan
+ langsung teori yang sudah dipelajari
+ 
+ Di bawah ini kalian akan membaca sebuah cerita tentang Pak Asep. Dari cerita tersebut, buatlah pada line di bawahnya sesuai instruksi
+ */
+
+
+// DATA TYPE
+/* Buatlah variable dan konstanta dari data sebagai berikut:
+ Pak Asep memesan sebuah buah di toko online yang hanya menerima uang dalam USD. 1 USD saat itu ekuivalen dengan Rp. 14500 (konstanta). Pak Asep memesan buah mangga sebanyak 23 buah. Status dari buah itu tersedia (isAvailable)(boolean).
+ */
+
+let usd: Int = 14500
+var fruitName: String = "Mangga"
+var amount: Int = 23
+var isAvailable: Bool = true
+
+
+
+// OPERATOR
+/*
+ Dari konstanta dan variable yang sudah dibuat di atas, buatlah perhitungan sesuai dengan cerita berikut:
+ Pak Asep pulang ke rumah membawa buah tersebut. Buah yang sudah dibeli tersebut harus dikurangi sebanyak 5 buah untuk diberikan ke Bu Nita. Lalu sisanya diberikan kepada anaknya yang masih SMA sebanyak 5 buah. Sisanya dimasukkan ke dalam kulkas yang sudah ada 4 buah mangga. Pak Asep harus mencocokkan apakah sudah memberikan buah dengan jumlah yang sama kepada Bu Nita dan anaknya. Berapakah jumlah sisa buah Pak Asep?
+ */
+
+var buNitaAmount: Int = 5
+var anakAmount: Int = 5
+var isSameAmount: Bool = buNitaAmount == anakAmount
+
+var sisaPakAsep: Int = amount - buNitaAmount - anakAmount
+var finalKulkas: Int = sisaPakAsep + 4
+
+
+
+// ARRAY
+/*
+ Buatlah sebuah array yang berisi jumlah buah yang sekarang dimiliki masing-masing dari mereka
+ */
+
+var buNita: [Int] = [buNitaAmount, anakAmount, finalKulkas]
